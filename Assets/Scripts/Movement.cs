0e68d2f52de33329b08c8613cc1e16bb0e43b476
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-10-20 16:01:19 +1100

    did basic player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Movement : MonoBehaviour
+{
+    [Header("Speed Vars")]
+    //value Variables
+    public float moveSpeed, rotateSpeed;
+    public float walkSpeed, runSpeed;
+
+    //Struct - Contains Multiple Variables (eg...3 floats)
+    private Vector3 _moveDir;
+    //Reference Variable
+
+    public CharacterController _charC;
+    public GameObject self;
+
+
+    private void Start()
+    {
+        _charC = GetComponent<CharacterController>();
+
+
+
+
+    }
+
+    private void Update()
+    {
+
+
+        Move();
+
+
+
+    }
+
+    private void Move()
+    {
+           //set speed
+
+
+
+
+
+
+
+            if (Input.GetKeyDown("e"))
+            {
+                moveSpeed = runSpeed;
+
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+                moveSpeed = 0;
+
+            }
+            else
+            {
+                moveSpeed = walkSpeed;
+
+            }
+        
+        _moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed;
+        
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, _moveDir, rotateSpeed, 0.0f);
+        self.transform.rotation = Quaternion.LookRotation(newDirection);
+        _charC.Move(_moveDir * Time.deltaTime);
+
+
+
+
+
+
+
+
+    }
+}
