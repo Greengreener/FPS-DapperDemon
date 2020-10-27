94bf71761965d67a7b32fd86d8b09ccfb142eccc
Greengreener <36747370+Greengreener@users.noreply.github.com>
2020-10-27 16:25:18 +1100

    StartedScripts

---
+++ Assets/Scripts/Oscar'sScripts/Gun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Monobehaviour
+{
+    Inventory inventory = gameObject.GetComponent<Inventory>();
+    float Damage;
+    float fireRate;
+    int ammoClipCurrent;
+    int ammoClipCapacity;
+
+    void Shoot()
+    {
+
+    }
+    void Reload()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Oscar'sScripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    float healthCurrent;
+    public float healthTotal;
+    void Start()
+    {
+        if (healthTotal == null)
+        {
+            Debug.LogError("Health total = null");
+        }
+        healthCurrent = healthTotal;
+    }
+    public void Heal(float _intakeHeal)
+    {
+        healthCurrent += _intakeHeal;
+        if (healthCurrent >= healthTotal)
+        {
+            healthCurrent = healthTotal;
+        }
+    }
+    public void Damage(float _intakeDamage)
+    {
+        healthCurrent -= _intakeDamage;
+    }
+}
