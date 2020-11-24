779fa95aef2ae9e29ecfc72d0b8e39a5c6f574b7
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-11-24 12:59:38 +1100

    added comments for movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,25 +17,31 @@ public class Movement : NetworkBehaviour
     private Vector3 _moveDir;
     //Reference Variable
     [SerializeField]
+    //bool that lets you move without going to lobby
     private bool forceMovement = false;
-
+    // character controller for moving the person
     public CharacterController _charC;
+    //sets what the player is
     public GameObject self;
-
+    //connects to the player health
     public Health playerHealth;
+    //sets how much fall damage you get
     public float fallDamage = 1;
 
     private void Start()
     {
+        //sets charecter controller
         _charC = GetComponent<CharacterController>();
+        //sets player health
         playerHealth = gameObject.GetComponent<Health>();
     }
 
     private void Update()
     {
-
+        //allows you to control player under certain conditions
         if (hasAuthority || forceMovement)
         {
+            //moves the player
             Move();
         }
 
@@ -44,12 +50,13 @@ public class Movement : NetworkBehaviour
     private void Move()
     {
         //set speed
-
+        //makes you run when you move
         if (Input.GetKeyDown("e"))
         {
             moveSpeed = runSpeed;
 
         }
+        //makes you stop when you crouch
         else if (Input.GetKeyDown("q"))
         {
             moveSpeed = 0;
@@ -60,7 +67,7 @@ public class Movement : NetworkBehaviour
             moveSpeed = walkSpeed;
 
         }
-
+        //the player jumps when you press j
         if (Input.GetKeyDown("j") && _charC.isGrounded == true)
         {
             _charC.Move(new Vector3(0, jumpForce, 0));
@@ -68,18 +75,21 @@ public class Movement : NetworkBehaviour
 
         else
         {
+            //the player falls to the ground at an accelerating rate
             if (_charC.isGrounded == false)
             {
                 gravity += Time.deltaTime * Time.deltaTime * 9.8f;
             }
             else
             {
+                //makes the player get hburt when hitting the ground hard enough
                 if (gravity >= 1)
                 {
                     Debug.Log("Player is hurt");
                     playerHealth.Damage(fallDamage);
                     gravity = 0.7f;
                 }
+             
                 else
                 {
                     gravity = 0.7f;
@@ -88,17 +98,22 @@ public class Movement : NetworkBehaviour
             }
 
         }
+        //sets the horizontal and vertical to the input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         //_moveDir = new Vector3(horizontal, -gravity * 2f, vertical) * moveSpeed;
+        //transform the vertical and horizontal into a vector3
         Vector3 forward = transform.forward * vertical;
         Vector3 right = transform.right * horizontal;
+        //combines the vertical and horizontal vectors into a single vector3
         _moveDir = (forward + right) * moveSpeed;
+        //adds gravity to the move dir
         _moveDir.y = -gravity * 2f;
 
         //Vector3 newDirection = Vector3.RotateTowards(transform.forward, _moveDir, rotateSpeed, 0.0f);
         //self.transform.rotation = Quaternion.LookRotation(newDirection);
+        // player moves in the direction of the move direction
         _charC.Move(_moveDir * Time.deltaTime);
 
     }
